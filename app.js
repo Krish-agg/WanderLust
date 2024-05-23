7efebf188646776d3999db3b9b1288d80dae0864
Krish Aggarwal <akrish620@gmail.com>
2024-05-24 00:13:17 +0530

    Add Project Files

---
+++ app.js
@@ -0,0 +1,114 @@
+if(process.env.NODE_ENV!="production"){
+    require('dotenv').config();
+}
+
+//console.log(process.env);
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const path=require("path");
+const ejsMate=require("ejs-mate");
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+const methodOverride=require("method-override");
+
+
+const ExpressError=require("./utils/ExpressError.js");
+const session=require("express-session");
+const MongoStore = require('connect-mongo');
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+const url=process.env.ATLASDB_URL;
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
+main().then(()=>{
+    console.log("connected to DB!");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(url);
+}    
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My New Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Goa",
+//         country:"India"
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was Saved");
+//     res.send("Sucessful!");
+
+// });
+const store=MongoStore.create({
+    mongoUrl:url,
+    crypto:{
+        secret:process.env.SECRET
+    },
+    touchAfter:24*60*60
+});
+store.on("error",()=>{
+    console.log("Mongo Session related Problem");
+})
+const sessionOptions={
+    store:store,
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+1000*60*60*24*3,
+        maxAge:1000*60*60*24*3,
+        httpOnly:true
+    }
+};
+
+
+app.use(session(sessionOptions));
+
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.currUser=req.user;
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
+})
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+
+
+app.all("*",(req,res)=>{
+    next(new ExpressError(404,"Page not Found!"));
+});
+app.use((err,req,res,next)=>{
+    let {status=500,message="Something went Wrong"}=err;
+    res.status(status).render("listings/error.ejs",{err});
+    //res.status(status).send(message);
+})
+
+
+app.listen(8080,(req,res)=>{
+    console.log("Server listening ...");
+})
+
---
+++ routes/listing.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const router=express.Router({mergeParams:true});
+const wrapAsync=require("../utils/wrapasync.js");
+const multer  = require('multer')
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage })
+const Listing=require("../models/listing.js");
+const {isLoggedin, isOwner, validateListing}=require("../middleware.js");
+const listingController=require("../controllers/listings.js");
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(isLoggedin,upload.single("listing[image]"),validateListing,wrapAsync(listingController.createListing));
+
+router.get("/new",isLoggedin,listingController.renderNewForm);
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedin,isOwner,upload.single("listing[image]"),validateListing,wrapAsync(listingController.updateListing))
+.delete(isLoggedin,isOwner,wrapAsync(listingController.deleteListing));
+
+
+
+
+
+//Edit Route
+router.get("/:id/edit",isLoggedin,isOwner,wrapAsync(listingController.renderEditForm));
+
+
+module.exports=router;
