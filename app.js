6a3dda550be55a92549a1f5dc44b32d148327466
Krish Aggarwal <akrish620@gmail.com>
2024-05-26 11:39:49 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
 
-app.all("*",(req,res)=>{
+app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not Found!"));
 });
 app.use((err,req,res,next)=>{
